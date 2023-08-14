1be8b615bdbb4007c14ced73045230b91d328b67
vselllena <nikolovaelena759@gmail.com>
2023-08-14 22:07:42 +0300

    fav

--- docs/demo1.151408fb.js
+++ docs/demo1.151408fb.js
@@ -5775,7 +5775,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53633" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62841" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
